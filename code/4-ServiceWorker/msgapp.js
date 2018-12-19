100a28a24898ed3d1a478cfad268881eed39d78d
Menglin Chen <chanmenglin@icloud.com>
2018-12-19 14:18:17 +0800

    update

--- code/4-ServiceWorker/msgapp.js
+++ code/4-ServiceWorker/msgapp.js
@@ -1,6 +1,15 @@
 if (navigator.serviceWorker) {
 	var sendButton = document.getElementById('send-msg-button');
 	var msg = document.getElementById('msg-import');
+
+	navigator.serviceWorker.ready.then(() => {
+		// I thought the page would be controlled at this point, thanks to clients.claim()
+		console.log('.ready resolved, and navigator.serviceWorker.controller is', navigator.serviceWorker.controller);
+		navigator.serviceWorker.addEventListener('controllerchange', () => {
+		  console.log('Okay, now things are under control. navigator.serviceWorker.controller is', navigator.serviceWorker.controller);
+		});
+	  });
+
 	sendButton.addEventListener('click', function () {
 		// 主页面发送信息到 Service Worker
 		navigator.serviceWorker.controller.postMessage(msg.value);
